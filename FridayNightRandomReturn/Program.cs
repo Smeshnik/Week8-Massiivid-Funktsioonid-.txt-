ba10d72963953cdad773559a5fe3416996817807
Smeshnik <ermachkovstas@mail.ru~>
2022-07-03 10:46:03 +0300

    first commit

---
+++ FridayNightRandomReturn/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace FridayNightRandomReturn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string randomFood, randomDrink, RandomMovie;
+
+            string[] foods = { "chicken wings", "pizza", "sushi", "popcorn", "salad" };
+            string[] drinks = { "apple juice", "coke", "fanta", "sprite" };
+            string[] movies = { "Top Gun", "The Batman", "Breaking Bad", "Joker" };
+            randomFood = PickRandomElementFromArray(foods);
+            randomDrink = PickRandomElementFromArray(drinks);
+            RandomMovie = PickRandomElementFromArray(movies);
+
+
+            Console.WriteLine($"Tonight you will have {randomFood} with {randomDrink} and watch {RandomMovie}.");
+            
+        }
+
+        private static int GenerateRandomIndex(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+            return randomIndex;
+        }
+
+        
+        private static string PickRandomElementFromArray(string[] someArray)
+        {
+            string randomElement = someArray[GenerateRandomIndex(someArray)];
+            return randomElement;
+
+            
+        }
+    }
+}
